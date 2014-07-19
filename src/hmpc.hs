d3074c2287b8f2fa069e71acb2a82d69c666ced2
Joachim Fasting <joachifm@fastmail.fm>
2014-07-19 10:42:43 +0200

    Factor out command defs

--- src/hmpc.hs
+++ src/hmpc.hs
@@ -32,40 +32,69 @@ main = do
 commands :: [(String, [String] -> IO ())]
 commands =
   [
-    ( "add", MPD.run . foldr1 (*>) . map (MPD.add . T.pack ) )
-  , ( "clear", \_ -> MPD.run MPD.clear )
-  , ( "current", \_ -> do
-         st <- MPD.run MPD.status
-         unless (MPD.statusPlaybackState st == "stop") $
-           T.putStrLn . formatCurrentSong =<< MPD.run MPD.currentSong
-    )
-  , ( "help", \_ -> putStr . unlines $ map fst commands )
-  , ( "ls", \xs -> MPD.run (MPD.listAll . maybe "" T.pack $ listToMaybe xs) >>= print )
-  , ( "next", \_ -> MPD.run MPD.next )
-  , ( "pause", \_ -> MPD.run MPD.pause )
-  , ( "play", \_ -> MPD.run (MPD.play Nothing) )
-  , ( "previous", \_ -> MPD.run MPD.previous )
-  , ( "random", \_ -> MPD.run (MPD.random True) )
-  , ( "shuffle", \_ -> MPD.run (MPD.shuffle Nothing) )
-  , ( "stop", \_ -> MPD.run MPD.stop )
-  , ( "status", \_ -> do
-         st <- MPD.run MPD.status
-         unless (MPD.statusPlaybackState st == "stop") $ do
-           cur <- MPD.run MPD.currentSong
-           T.putStrLn (formatCurrentSong cur)
-         T.putStrLn (formatStatus st)
-    )
+    ( "add", add )
+  , ( "clear", clear )
+  , ( "current", current )
+  , ( "help", help )
+  , ( "ls", ls )
+  , ( "next", next )
+  , ( "pause", pause )
+  , ( "play", play )
+  , ( "previous", previous )
+  , ( "random", random )
+  , ( "shuffle", shuffle )
+  , ( "stop", stop )
+  , ( "status", status )
   ]
-  where
-    formatCurrentSong si =
-      let Just artist = si `MPD.viewTag` "Artist"
-          Just title  = si `MPD.viewTag` "Title"
-      in T.unwords [ artist, "-", title ]
 
-    formatStatus st = T.intercalate "\t"
-      [ "volume: "  <> MPD.statusVolume st
-      , "repeat: "  <> MPD.statusRepeatEnabled st
-      , "random: "  <> MPD.statusRandomEnabled st
-      , "single: "  <> MPD.statusSingleEnabled st
-      , "consume: " <> MPD.statusConsumeEnabled st
-      ]
+------------------------------------------------------------------------
+
+add = MPD.run . foldr1 (*>) . map (MPD.add . T.pack)
+
+clear _ = MPD.run MPD.clear
+
+current _ = do
+  st <- MPD.run MPD.status
+  unless (MPD.statusPlaybackState st == "stop") $
+    T.putStrLn . formatCurrentSong =<< MPD.run MPD.currentSong
+
+help _ = putStr . unlines $ map fst commands
+
+ls xs = print =<<
+  MPD.run (MPD.listAll . maybe "" T.pack $ listToMaybe xs)
+
+next _ = MPD.run MPD.next
+
+pause _ = MPD.run MPD.pause
+
+play _ = MPD.run (MPD.play Nothing)
+
+previous _ = MPD.run MPD.previous
+
+random _ = MPD.run (MPD.random True)
+
+shuffle _ = MPD.run (MPD.shuffle Nothing)
+
+status _ = do
+  st <- MPD.run MPD.status
+  unless (MPD.statusPlaybackState st == "stop") $ do
+    cur <- MPD.run MPD.currentSong
+    T.putStrLn (formatCurrentSong cur)
+  T.putStrLn (formatStatus st)
+
+stop _ = MPD.run MPD.stop
+
+------------------------------------------------------------------------
+
+formatCurrentSong si =
+  let Just artist = si `MPD.viewTag` "Artist"
+      Just title  = si `MPD.viewTag` "Title"
+  in T.unwords [ artist, "-", title ]
+
+formatStatus st = T.intercalate "\t" [
+    "volume: "  <> MPD.statusVolume st
+  , "repeat: "  <> MPD.statusRepeatEnabled st
+  , "random: "  <> MPD.statusRandomEnabled st
+  , "single: "  <> MPD.statusSingleEnabled st
+  , "consume: " <> MPD.statusConsumeEnabled st
+  ]
